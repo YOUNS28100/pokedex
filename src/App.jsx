72277c687d48abb0c8d467a076dbf3b5e09b34f8
Jean-Francois <hafidjeanfrancois@gmail.com>
2023-10-11 23:27:50 +0200

    state.1 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import PokemonCard from "./components/PokemonCard.jsx";
 
 const pokemonList = [
@@ -7,16 +8,63 @@ const pokemonList = [
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
   },
   {
-    name: "mew",
+    name: "Salamèche",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "Carapuce",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "Pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+  {
+    name: "Mew",
   },
 ];
 
 function App() {
-  return (
-    <>
-      <PokemonCard imgSrc={pokemonList[0].imgSrc} name={pokemonList[0].name} />
-    </>
-  );
+  const [index, setIndex] = useState(0);
+
+  const next = () => setIndex(index + 1);
+  const previous = () => setIndex(index - 1);
+
+  if (index == 0) {
+    return (
+      <>
+        <PokemonCard
+          imgSrc={pokemonList[index].imgSrc}
+          name={pokemonList[index].name}
+        />
+        <button onClick={next}>Suivant</button>
+      </>
+    );
+  } else if (index == pokemonList.length - 1) {
+    return (
+      <>
+        <PokemonCard
+          imgSrc={pokemonList[index].imgSrc}
+          name={pokemonList[index].name}
+        />
+        <button onClick={previous}>Précédent</button>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <PokemonCard
+          imgSrc={pokemonList[index].imgSrc}
+          name={pokemonList[index].name}
+        />
+        <button onClick={previous}>Précédent</button>
+        <button onClick={next}>Suivant</button>
+      </>
+    );
+  }
 }
 
 export default App;
