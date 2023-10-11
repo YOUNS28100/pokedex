8d6fcb75daea397ad98306059f074f332048b637
Jean-Francois <hafidjeanfrancois@gmail.com>
2023-10-11 09:58:10 +0200

    update component 1

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,31 +1,13 @@
-const pokemonList = [
-  {
-    name: "Bulbizarre",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
-
 function PokemonCard() {
-  const pokemon = pokemonList[0];
-  if (pokemon.imgSrc == undefined) {
-    return (
-      <figure>
-        <p>???</p>
-        <figcaption>{pokemon.name}</figcaption>
-      </figure>
-    );
-  } else {
-    return (
-      <figure>
-        <img src={pokemon.imgSrc} alt={pokemon.name} />
-        <figcaption>{pokemon.name}</figcaption>
-      </figure>
-    );
-  }
+  return (
+    <figure>
+      <img
+        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+        alt="Bulbizarre"
+      />
+      <figcaption>Bulbizarre</figcaption>
+    </figure>
+  );
 }
 
 export default PokemonCard;
